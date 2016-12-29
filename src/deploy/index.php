63ce727e7679e6f0c744e020c9cb3fd1bc3ee4de
David Twigger <davidtwigger@gmail.com>
2016-12-29 08:54:21 +0100

    add deploy hook

--- src/deploy/index.php
+++ src/deploy/index.php
@@ -1,7 +1,10 @@
 <?php
-//$secret = '0mh=1bi41#!sbve_q_mg@6c+2rvz-=6g8@4o!yhsmu)czye1)+';
-//$signature = hash_hmac('sha1', $policy, $secret_key, true);
-if(true) {
+$secret = '0mh=1bi41#!sbve_q_mg@6c+2rvz-=6g8@4o!yhsmu)czye1)+';
+$post = file_get_contents('php://input');
+list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+$signature = hash_hmac($algo, $post, $secret);
+
+if($signature === $hash) {
     exec('./pull.sh', $output);
-    echo $output[0];
+    echo "\n" . $output[0];
 }
